7fd3c99eda8ac0134d36f3f5080ed16a93a16fd8
OlegTsvetov <brainstormmedia@ya.ru>
2022-03-11 13:41:30 +0500

    Добавлен класс .simple в заявление.

--- dist/js/lk_send_request.js
+++ dist/js/lk_send_request.js
@@ -488,5 +488,13 @@ $(document).ready(function () {
         $('input[name="redirect"]').val('newrequesttp');
         break;
     }
+  });
+  $.ajax({
+    url: "/lktp/getSimpleJson/",
+    success: function success(data) {
+      if (data == 'true') {
+        $("[name='infmaxparam4']," + "[name='infmaxparam3']," + "[name='connectloadparamdata_value2']," + "[name='addconnectloadparamdata_value_05']," + "[name='addconnectloadparamdata_value_08']," + "[name='addconnectloadparamdata_value_02']," + "[name='addconnectloadparamdata_value_07']," + "[name='connectloadparamdata_value2_2']," + "[name='addconnectloadparamdata_value_06']," + "[name='connectloadparamdata_value3']," + "[name='connectloadparamdata_value1'].mh," + "[name='connectloadparamdata_value1_2'].mh," + "#needToHide," + "#typeOfConnectionObject label:contains('Реконструкция') input," + "[name='infmaxparam2']").parent().hide(); //).parent().addClass('hidden');
+      }
+    }
   }); //#endregion
 });
