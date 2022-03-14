8b43b802f0ce7773e21474776218d6834f1a4c79
OlegTsvetov <brainstormmedia@ya.ru>
2022-03-14 23:56:26 +0500

    Добавлен инит масок в новых очередях, слайд 4.

--- dist/js/lk_send_request.js
+++ dist/js/lk_send_request.js
@@ -248,6 +248,7 @@ $(document).ready(function () {
       pasteNameSuffixes(newNode);
       renderNewNode(newNode);
       initPseudoSelects(newNode.querySelector('.__select'));
+      initMasks(newNode);
       initColdWaterSupply(newNode);
       initDrainage(newNode);
       initAddressConcatination(newNode); // addNewSlide(newNode)
@@ -331,40 +332,44 @@ $(document).ready(function () {
 
   if (document.querySelector('.datepicker_input')) initDatepickers(); // маски
 
-  if (document.querySelector('.datepicker_input')) $('.datepicker_input').mask("99.99.9999", {
-    autoclear: false
-  });
-  if (document.querySelector('.snils_input')) $('.snils_input').mask("999-999-999 99", {
-    autoclear: false
-  });
-  if (document.querySelector('.passport_input')) $('.passport_input').mask("99 99 / 999999", {
-    autoclear: false
-  });
-  if (document.querySelector('.passport_serial_input')) $('.passport_serial_input').mask("99 99", {
-    autoclear: false
-  });
-  if (document.querySelector('.passport_number_input')) $('.passport_number_input').mask("999999", {
-    autoclear: false
-  });
-  if (document.querySelector('.phone_input')) $('.phone_input').mask("(999) 999-9999", {
-    autoclear: false
-  });
-  if (document.querySelector('.tin_ul_input')) $('.tin_ul_input').mask("9999999999", {
-    autoclear: false
-  });
-  if (document.querySelector('.tin_fl_input')) $('.tin_fl_input').mask("999999999999", {
-    autoclear: false
-  });
-  if (document.querySelector('.tin_e_input')) $('.tin_e_input').mask("999999999999", {
-    autoclear: false
-  });
-  if (document.querySelector('.integer_input')) $('.integer_input').on('input', function () {
-    $(this).val($(this).val().replace(/[^0-9]/g, ''));
-  });
-  if (document.querySelector('.float_input')) $('.float_input').keypress(function (e) {
-    var trigger = (e.which != 46 || $(this).val().indexOf('.') != -1) && (e.which < 48 || e.which > 57);
-    if (trigger) e.preventDefault();
-  }); // Блок "Являюсь представителем"
+  function initMasks(parentNode) {
+    if (parentNode.querySelector('.datepicker_input')) $('.datepicker_input').mask("99.99.9999", {
+      autoclear: false
+    });
+    if (parentNode.querySelector('.snils_input')) $('.snils_input').mask("999-999-999 99", {
+      autoclear: false
+    });
+    if (parentNode.querySelector('.passport_input')) $('.passport_input').mask("99 99 / 999999", {
+      autoclear: false
+    });
+    if (parentNode.querySelector('.passport_serial_input')) $('.passport_serial_input').mask("99 99", {
+      autoclear: false
+    });
+    if (parentNode.querySelector('.passport_number_input')) $('.passport_number_input').mask("999999", {
+      autoclear: false
+    });
+    if (parentNode.querySelector('.phone_input')) $('.phone_input').mask("(999) 999-9999", {
+      autoclear: false
+    });
+    if (parentNode.querySelector('.tin_ul_input')) $('.tin_ul_input').mask("9999999999", {
+      autoclear: false
+    });
+    if (parentNode.querySelector('.tin_fl_input')) $('.tin_fl_input').mask("999999999999", {
+      autoclear: false
+    });
+    if (parentNode.querySelector('.tin_e_input')) $('.tin_e_input').mask("999999999999", {
+      autoclear: false
+    });
+    if (parentNode.querySelector('.integer_input')) $('.integer_input').on('input', function () {
+      $(this).val($(this).val().replace(/[^0-9]/g, ''));
+    });
+    if (parentNode.querySelector('.float_input')) $('.float_input').keypress(function (e) {
+      var trigger = (e.which != 46 || $(this).val().indexOf('.') != -1) && (e.which < 48 || e.which > 57);
+      if (trigger) e.preventDefault();
+    });
+  }
+
+  initMasks(document); // Блок "Являюсь представителем"
 
   function initCheckRepresentative() {
     var representativeBlock = document.querySelector('.representative');
