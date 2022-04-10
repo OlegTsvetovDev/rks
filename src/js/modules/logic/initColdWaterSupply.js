871e6d6596888bbcfa71045045a05cb0e990a39e
Пятисоцкая Инга <pis@gelicon.biz>
2022-04-10 22:17:21 +0500

    Обязательность таблицы на 4-ом слайде + убрала опечатывание [когда-то забыла закоммитить]

--- src/js/modules/logic/initColdWaterSupply.js
+++ src/js/modules/logic/initColdWaterSupply.js
@@ -8,10 +8,17 @@ function initColdWaterSupply(node) {
   let isChecked = coldWater.checked
   const isDisabled = coldWater.disabled
   const toggle = node.querySelector('.cold_water_supply_toggle')
+  const water_sources_label = document.querySelector('.other_water_sources h6');
 
   // проверка начального состояния чекбокса
-  if (isChecked) toggle.classList.remove('hidden')
-  if (!isChecked) toggle.classList.add('hidden')
+  if (isChecked) {
+    toggle.classList.remove('hidden')
+    if (water_sources_label) water_sources_label.classList.add('required')
+  }
+  if (!isChecked) {
+    toggle.classList.add('hidden')
+    if (water_sources_label) water_sources_label.classList.remove('required')
+  }
 
   if (isDisabled) return
 
@@ -20,9 +27,11 @@ function initColdWaterSupply(node) {
 
     if (isChecked) {
       toggle.classList.remove('hidden')
+      if (water_sources_label) water_sources_label.classList.add('required')
       changeSliderHeight()
     } else {
       toggle.classList.add('hidden')
+      if (water_sources_label) water_sources_label.classList.remove('required')
       changeSliderHeight()
     }
 
